90b258682138e2e8b91910928c35679c8a5b25da
李俊恒 <YxhHLi@163.com>
2019-07-23 16:57:59 +0800

    更新Kit库到最新版本

---
+++ WYASwiftMaterialUnitTests/WYASwiftMaterialUnitTests.swift
@@ -0,0 +1,34 @@
+//
+//  WYASwiftMaterialUnitTests.swift
+//  WYASwiftMaterialUnitTests
+//
+//  Created by 李俊恒 on 2019/7/11.
+//  Copyright © 2019 WeiYiAn. All rights reserved.
+//
+
+import XCTest
+
+class WYASwiftMaterialUnitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(true,"成功")
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
